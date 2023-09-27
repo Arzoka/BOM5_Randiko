52cc77382f0a0459afa464f7cc678b4483cdd856
Melvin <melvin.ja@foxiko.com>
2023-09-27 12:21:23 +0200

    Updated LanguageHandler, Contact.html, header, styles.sass, made everything responsive to 300px

--- web/js/LanguageHandler.js
+++ web/js/LanguageHandler.js
@@ -1,17 +1,16 @@
-let current_data;
-
-fetch("data.json")
-  .then((response) => response.json())
-  .then((data) => {
-    let language = localStorage.getItem("language");
-    if (!language || language !== "dutch") {
-      doLanguageInput(data.en);
-    } else doLanguageInput(data.nl);
-  })
-  .catch((error) => console.log(error));
+const doLanguageCheck = () => {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      let language = localStorage.getItem("language");
+      if (!language || language !== "dutch") {
+        doLanguageInput(data.en);
+      } else doLanguageInput(data.nl);
+    })
+    .catch((error) => console.log(error));
+};
 
 const doLanguageInput = (data) => {
-  console.log(data);
   if (document.getElementById("SDG_Overview")) {
     document.getElementById("SDG_Overview").innerHTML = data.index.Overview;
     document.getElementById("play_SDG").innerHTML = data.index.PlaySDG;
@@ -20,3 +19,14 @@ const doLanguageInput = (data) => {
     document.getElementById("contact_title").innerHTML = data.contact.Title;
   }
 };
+
+const setLanguage = (lang) => {
+  if (lang == "dutch") {
+    localStorage.setItem("language", "dutch");
+  } else {
+    localStorage.setItem("language", "english");
+  }
+  doLanguageCheck();
+};
+
+doLanguageCheck();
